4dcf3d3bc5da8d9d9affea5515a8216f0bcb8fc4
keven518 <957314724@qq.com>
2020-12-01 17:56:18 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="container">
+    <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+    <h1 class="title">
+      Universal Vue.js Application Framework
+    </h1>
+    <el-button type="primary">柯文我爱你</el-button>
+    <br/>
+    <br/>
+    <nuxt-link class="button" to="/about">
+      About page
+    </nuxt-link>
+  </section>
+</template>
+
+<style scoped>
+.title
+{
+  margin: 50px 0;
+}
+</style>
